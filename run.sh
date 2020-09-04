686f4100fbd44a153e16c29511d532f8df8f9510
mrkuz <markus@bitsandbobs.net>
2020-09-04 14:49:40 +0200

    Add home-manager

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # ansible-playbook -C -K -i inventory.yml playbook.yml
-ansible-playbook -i inventory.yml playbook.yml
+ansible-playbook -C -i inventory.yml playbook.yml
